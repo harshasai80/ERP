578ac18746cf1d265d4d0485ef5f05c945f61eb9
kaif <nawaz@gmail.com>
2025-03-12 19:41:09 +0530

    add stu done

--- college-erp-management-system/college_erp_frontend/college-erp/src/components/faculty/SyllabusTab.js
+++ college-erp-management-system/college_erp_frontend/college-erp/src/components/faculty/SyllabusTab.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { syllabusData } from "./Data/demoData";
+import { syllabusData } from "../Data/demoData";
 import Alert from "./Alert";
 
 function SyllabusTab() {
